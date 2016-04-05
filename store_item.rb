9452849683a11090d6d3068769a41c5b12e661c9
Chris Bon <chrisbon315@gmail.com>
2016-04-04 20:42:59 -0700

    First commit

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+banana = {color: yellow, price: 0.5, category: fruit}
+apple = {color: red, price: 0.25, category: fruit}
+zucchini = {color: green, price: 0.4, category: vegetable}
+beef = {color: brown, price: 5.2, category: meat}                            
